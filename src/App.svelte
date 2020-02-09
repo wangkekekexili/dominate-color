9e1d28cb77374dd568ea8c5db39f5ee3b0b86f78
Ke Wang <wangkekekexili@gmail.com>
2020-02-09 21:09:31 +0800

    Some initial garbage.

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+  import { onMount } from "svelte";
+  import { canvasImageDataToRGBArray, getAverageColor } from "./color.svelte";
+
+  let input;
+  let c;
+
+  onMount(function() {
+    input.addEventListener("change", function() {
+      const image = input.files[0];
+      const imageURL = URL.createObjectURL(image);
+
+      const i = new Image();
+      i.addEventListener("load", function() {
+        c.width = i.width;
+        c.height = i.height;
+        const ctx = c.getContext("2d");
+        ctx.drawImage(i, 0, 0);
+        const averageColor = getAverageColor(
+          canvasImageDataToRGBArray(
+            ctx.getImageData(0, 0, i.width, i.height).data
+          )
+        );
+        const body = document.querySelector("body");
+        body.style.backgroundColor = `rgba(${averageColor.r},${averageColor.g},${averageColor.b},1)`;
+      });
+      i.src = imageURL;
+    });
+  });
+</script>
+
+<input bind:this={input} type="file" accept="image/*" />
+<div style="height: 10px;">
+  <canvas bind:this={c} />
+</div>
---
+++ src/color.svelte
@@ -0,0 +1,29 @@
+<script context="module">
+  export function canvasImageDataToRGBArray(data) {
+    const rgbs = [];
+    for (let i = 0; i < data.length; i += 4) {
+      rgbs.push({
+        r: data[i],
+        g: data[i + 1],
+        b: data[i + 2]
+      });
+    }
+    return rgbs;
+  }
+
+  export function getAverageColor(colors) {
+    let totalR = 0;
+    let totalG = 0;
+    let totalB = 0;
+    colors.forEach(color => {
+      totalR += color.r * color.r;
+      totalG += color.g * color.g;
+      totalB += color.b * color.b;
+    });
+    return {
+      r: Math.floor(Math.sqrt(totalR / colors.length)),
+      g: Math.floor(Math.sqrt(totalG / colors.length)),
+      b: Math.floor(Math.sqrt(totalB / colors.length))
+    };
+  }
+</script>
